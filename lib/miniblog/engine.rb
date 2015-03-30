40a2d08740de6eaa1aa99dfcc174636feff8c0da
David Padilla <david@easybroker.com>
2015-03-29 22:13:19 -0600

    Load decorators

--- lib/miniblog/engine.rb
+++ lib/miniblog/engine.rb
@@ -5,5 +5,11 @@ module Miniblog
     initializer :assets do |config|
       Rails.application.config.assets.precompile += %w(miniblog.css miniblog.js)
     end
+
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
